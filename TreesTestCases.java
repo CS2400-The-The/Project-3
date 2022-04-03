a4df150382abed802af04e207373ca751df49d23
Daniel Pasion <dnpaxion@gmail.com>
2022-04-02 17:14:28 -0700

    Todo: fill in test cases

---
+++ TreesTestCases.java
@@ -0,0 +1,48 @@
+package com.jamesjhansen;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Current test case ideas:
+ * 
+ * for post order:
+ * 1. Empty Tree
+ * 2. Tree with one node (root)
+ * 3. Tree with two nodes, root and left
+ * 4. Tree with two nodes, root and right
+ * 5. Tree with only node and right children
+ * 6. Tree with only node and left children
+ * 7. Complete, but not full tree
+ * 8. Full tree,
+ * 9. Random mix
+ */
+public class TreesTestCases extends BinaryTree{
+
+    @test
+    void postOrderTraverse(){
+
+    }
+
+    @test
+    void postOrderTraverse_callbinaryNodeMethod(){
+
+    }
+
+    @test getHeight(){
+    
+    }
+
+    @test getHeight_callBinaryNodeMethod(){
+
+    }
+
+    @test getNumberOfNodes() {
+
+    }
+
+    @test getNumberOfNodes_callBinaryNodeMethod() {
+
+    }
+    
+}
